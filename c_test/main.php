c01ca76fc0c0452d371a8ce3da5e6a95f898ff5a
robinhur <robinhur@naver.com>
2017-09-10 12:46:49 +0900

    main3

--- c_test/main.php
+++ c_test/main.php
@@ -9,7 +9,7 @@
     $user_id = $_COOKIE['user_id'];
     $user_pw = $_COOKIE['user_pw'];
 
-    echo "<p>안녕하세요, $user_name님</p>";
+    echo "<p>안녕하세요, $user_id</p>";
     echo "<p>당신의 비밀번호는 " + strlen($user_pw) + "자리 입니다</p>";
 
     echo "<p><a href='logout.php'>로그아웃</a></p>";
