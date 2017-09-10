a608b4d127d3f83a9813869dccc3997dd76421fd
robinhur <robinhur@naver.com>
2017-09-10 11:37:34 +0900

    if statement added in test2.php

--- php_test/test2.php
+++ php_test/test2.php
@@ -5,6 +5,14 @@
     <body>
         <?php
             echo $_SERVER['HTTP_USER_AGENT'];
+
+            if (strpos($_SERVER['HTTP_USER_AGENT'], 'Chrome') == TRUE) {
+                echo 'Now, you are in CHROME';
+            } elseif (strpos($_SERVER['HTTP_USER_AGENT'], 'Safari') == TRUE) {
+                echo 'Now, you are in SAFARI';
+            } else {
+                echo 'Now, you are in MS IE (maybe??)';
+            }
         ?>
     </body>
 </html>
