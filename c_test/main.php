e5b24b190decd27e668338b0203138666ce2052e
robinhur <robinhur@naver.com>
2017-09-10 12:48:39 +0900

    main5

--- c_test/main.php
+++ c_test/main.php
@@ -9,7 +9,7 @@
     $user_id = $_COOKIE['user_id'];
     $user_pw = $_COOKIE['user_pw'];
 
-    echo "<p>안녕하세요, $user_id님</p>";
+    echo "<p>안녕하세요, $user_id 님</p>";
     echo "<p>당신의 비밀번호는 ";
     echo strlen($user_pw);
     echo "자리 입니다</p>";
