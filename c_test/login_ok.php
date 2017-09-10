7322ddf6526e19598d066ed12c9ee5a0ad4999df
robinhur <robinhur@naver.com>
2017-09-10 12:08:39 +0900

    login test 1

--- c_test/login_ok.php
+++ c_test/login_ok.php
@@ -1,7 +1,26 @@
 <?php
+$member = array(
+                'user1' => array('pw' => 'ppp1', 'name' => 'NO1'),
+                'user2' => array('pw' => 'ppp2', 'name' => 'NO2'),
+                'user3' => array('pw' => 'ppp3', 'name' => 'NO3')
+            );
+
+if (!isset($member[$_GET['user_id']])) {
+    echo "<script>
+            alert('아이디가 존재하지 않습니다');
+            history.back();
+         </script>
+         ";
+}
+
+
+
+
+
     if (isset($_GET['user_id']) && isset($_GET['user_pw'])) {
         echo 'ID : ';
         echo $_GET['user_id'];
         echo '</br>PW : ';
         echo $_GET['user_pw'];
     }
+?>
