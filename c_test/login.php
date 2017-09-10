174e022abab4848ad19541b058a5c309d639e1f4
robinhur <robinhur@naver.com>
2017-09-10 11:50:50 +0900

    cookie start

---
+++ c_test/login.php
@@ -0,0 +1,7 @@
+<!DOCTYPE html>
+<meta charset="utf-8" />
+<form method='post' action='login_ok.php'>
+    <input type='text' name='user_id' />
+    <input type='password' name='user_pw' />
+    <input type='submit' value='로그인' />
+</form>
