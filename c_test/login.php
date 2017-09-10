13f35f26ab4c55feeb2f016524308daea2f868ce
robinhur <robinhur@naver.com>
2017-09-10 12:00:09 +0900

    get temp

--- c_test/login.php
+++ c_test/login.php
@@ -1,6 +1,6 @@
 <!DOCTYPE html>
 <meta charset="utf-8" />
-<form method='post' action='login_ok.php'>
+<form method='get' action='login_ok.php'>
     <input type='text' name='user_id' />
     <input type='password' name='user_pw' />
     <input type='submit' value='로그인' />
