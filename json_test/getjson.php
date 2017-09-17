276f9c5380a467a10a2d0c969fe46bd53ea9298a
HuZA_LAPTOP <sgrobinhur@gmail.com>
2017-09-17 11:45:25 +0900

    array!!

--- json_test/getjson.php
+++ json_test/getjson.php
@@ -15,6 +15,35 @@
 	mysqli_set_charset($link,"utf8");
 	
 	
+	$sql_query = "select * from Person";
+	
+	$raw_result = mysqli_query($link, $sql_query);
+	echo $raw_result;
+	echo "<br>";
+	
+	$result_array = array();
+	if ($raw_result) {
+		
+		while($row = mysqli_fetch_array($raw_result)) {
+			array_push($result_array, 
+						array(
+								'id'=>$row[0],
+								'name'=>$row[1],
+								'address'=>$row[2]
+						)
+			);
+		}
+		
+		echo $result_array;
+		echo "<br>";
+		
+		echo print_r($result_array);
+		
+	}
+	
+	
+	
+	
 	mysqli_close($link);
 	
 ?>
